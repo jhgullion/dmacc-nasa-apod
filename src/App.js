8f5575d3c96483f506e992591b20122f180d4ffe
Gully <gullydsm@aol.com>
2022-12-01 15:53:49 -0600

    Showing picture of the day with loading indicator

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import {getPictureOfTheDay} from "./api";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
   //useState is a hook, used previously
+  //pod = picture of the day
+  const [pod,setPod] = useState(null);
   //useEffect is a hook to trigger actions
   //useEffect is a function
   // takes two parameters - first a function that will happen when the effect triggers
   //  -two the dependency array tells the effect when to run 
+
   useEffect(() => {
     //useEffect hooks cannot call out to async calls directly
     const getImage = async () => {
       const response = await getPictureOfTheDay();
       console.log("Parsed response body:", response);
+
+      setPod(response);
     }
 
     getImage();
   }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        pod ?
+          <>
+            <h1>{pod.title}</h1>
+            <p>{pod.explanation}</p>
+            <img src={pod.hdurl} alt={pod.title}></img>         
+          </>
+          :
+          <h1>Loading...</h1>        
+      }
     </div>
   );
 }
